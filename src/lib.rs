de7e1b88bd41408d59e75ab79607b27629585146
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-10-10 16:12:11 +0200

    Updated toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
 #![feature(return_position_impl_trait_in_trait)]
 
-pub mod traits;
-pub mod sigmoid_decomposition;
-pub mod basic_iterative_decomposition;
 pub mod basic_decomposition;
-pub mod utils;
+pub mod basic_iterative_decomposition;
 pub mod macros;
 pub mod numpy_decomposition;
+pub mod sigmoid_decomposition;
+pub mod traits;
+pub mod utils;
+
+use pyo3::{pymodule, types::PyModule, PyResult, Python};
+pub use sigmoid_decomposition::SigmoidDecomposition;
+
+#[pymodule]
+pub fn dimensional_reduction(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<SigmoidDecomposition>()?;
+    Ok(())
+}
